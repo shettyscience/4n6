cf5cea52d256ea73d44b0b965f9c3c56d8645547
ka4n6 <68684152+ka4n6@users.noreply.github.com>
2021-02-14 11:52:17 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -48,6 +48,35 @@ def kannada():
     sorted_pages=sorted(posts, reverse=True, key=lambda page: datetime.strptime(page.meta["date"], "%d %b %y"))
     return render_template('kannada.html', pages=sorted_pages)
    
+@app.route("/dfir")
+def dfir():
+    posts = [p for p in pages if "tagdfir" in p.meta]
+    sorted_pages=sorted(posts, reverse=True, key=lambda page: datetime.strptime(page.meta["date"], "%d %b %y"))
+    return render_template('dfir.html', pages=sorted_pages)
+
+@app.route("/osint")
+def osint():
+    posts = [p for p in pages if "tagosint" in p.meta]
+    sorted_pages=sorted(posts, reverse=True, key=lambda page: datetime.strptime(page.meta["date"], "%d %b %y"))
+    return render_template('osint.html', pages=sorted_pages)
+
+@app.route("/dweb")
+def dweb():
+    posts = [p for p in pages if "tagdarkweb" in p.meta]
+    sorted_pages=sorted(posts, reverse=True, key=lambda page: datetime.strptime(page.meta["date"], "%d %b %y"))
+    return render_template('dweb.html', pages=sorted_pages)
+
+@app.route("/trg")
+def trg():
+    posts = [p for p in pages if "tagtraining" in p.meta]
+    sorted_pages=sorted(posts, reverse=True, key=lambda page: datetime.strptime(page.meta["date"], "%d %b %y"))
+    return render_template('training.html', pages=sorted_pages)
+
+@app.route("/basics")
+def basics():
+    posts = [p for p in pages if "tagbasics" in p.meta]
+    sorted_pages=sorted(posts, reverse=True, key=lambda page: datetime.strptime(page.meta["date"], "%d %b %y"))
+    return render_template('basics.html', pages=sorted_pages)
 
 if __name__ == '__main__':
     app.run()
